fa008a8a0813350c52c5c9e991a8c2a99ff0fa71
Dowglas Maia <dowglasmaia@gmail.com>
2019-03-24 16:40:01 -0300

    Impl-> Pegando Rota ativa com subscribe

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-oferta',
@@ -15,8 +16,13 @@ export class OfertaComponent implements OnInit {
   }
 
   ngOnInit() {
-    /* pegando os dados da roa ativa, o parametro deve ser o da Rota, no caso aqui o (oferta/:id) */
-    console.log('ID recuperado da rota:', this.route.snapshot.params['id']);
+    /* pegando os dados da rota ativa com o 'SNAPSHORT', o parametro deve ser o da Rota, no caso aqui o (oferta/:id) */
+   // console.log('ID recuperado da rota:', this.route.snapshot.params['id']);
+
+    /* pegando os dados da rota ativa com o 'SUBSCRIBE', passando o id como parametro*/
+    this.route.params.subscribe((parametro : any) => {
+      console.log(parametro.id);
+    })
   }
 
 }
