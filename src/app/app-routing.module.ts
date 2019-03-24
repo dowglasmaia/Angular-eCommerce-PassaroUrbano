cd9abcd1b412ac1774752905980a7729c9782457
Dowglas Maia <dowglasmaia@gmail.com>
2019-03-23 23:27:10 -0300

    Padranizando Rotas de Navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { RestauranteComponent } from './restaurante/restaurante.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-
+  { path: '', component: HomeComponent },
+  { path: 'diversao', component: DiversaoComponent },
   { path: 'restaurante', component: RestauranteComponent },
 ];
 
@@ -15,4 +19,4 @@ export class AppRoutingModule {
 
 
 
- }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,16 @@ import { HttpClientModule }    from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopoComponent } from './topo/topo.component';
+
 import { HomeComponent } from './home/home.component';
-import { RodapeComponent } from './rodape/rodape.component';
 import { OfertaService } from './services/ofertas.service';
 import { RestauranteComponent } from './restaurante/restaurante.component';
 import { DiversaoComponent } from './diversao/diversao.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopoComponent,
-    HomeComponent,
-    RodapeComponent,
+    AppComponent, 
+    HomeComponent,  
     RestauranteComponent,
     DiversaoComponent
   ],
