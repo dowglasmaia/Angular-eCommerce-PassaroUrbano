3ea42d8f2ca6ae394025296730b4614652585897
Dowglas Maia <dowglasmaia@gmail.com>
2019-03-24 14:04:47 -0300

    Impl-> Pegando Rota Ativa, com Snapshort

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-oferta',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+ // private route: ActivatedRoute;
+
+  constructor(private route: ActivatedRoute) {
+    //this.route = route;
+  }
 
   ngOnInit() {
+    /* pegando os dados da roa ativa, o parametro deve ser o da Rota, no caso aqui o (oferta/:id) */
+    console.log('ID recuperado da rota:', this.route.snapshot.params['id']);
   }
 
 }
