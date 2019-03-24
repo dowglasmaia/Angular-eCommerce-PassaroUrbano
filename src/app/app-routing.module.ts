689d693b9149288423f7f9bd2aced934ab13ee57
Dowglas Maia <dowglasmaia@gmail.com>
2019-03-23 22:57:45 -0300

    Impl-> Component, Restaurante e Diversão

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RestauranteComponent } from './restaurante/restaurante.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'restaurante', component: RestauranteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { OfertaService } from './services/ofertas.service';
+import { RestauranteComponent } from './restaurante/restaurante.component';
+import { DiversaoComponent } from './diversao/diversao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     HomeComponent,
-    RodapeComponent
+    RodapeComponent,
+    RestauranteComponent,
+    DiversaoComponent
   ],
   imports: [
     BrowserModule,
