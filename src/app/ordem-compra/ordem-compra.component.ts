df3233e159f78b5692e0aa161fb29f73944da604
Dowglas Maia <dowglasmaia@gmail.com>
2019-04-28 14:33:52 -0300

    Fazendo controle dos inputs do Formulario, com Two-Way_Binding

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  endereco: string = '';
+  numero: string = '';
+  complemento: string = '';
+  formaPagamento: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  public atualizaEndereco(endereco: string): void {
+    this.endereco = endereco;
+    console.log(this.endereco)
+  }
+
+  public atualizaNumero(numero: string): void {
+    this.numero = numero;
+    console.log(numero)
+  }
+
+  public atualizaComplemento(complemento: string): void {
+    this.complemento = complemento;
+    console.log(this.complemento)
+  }
+
+  public atualizaFormaPgto(formaPgto: string): void {
+    this.formaPagamento = formaPgto;
+    console.log(this.formaPagamento)
+  }
 }
