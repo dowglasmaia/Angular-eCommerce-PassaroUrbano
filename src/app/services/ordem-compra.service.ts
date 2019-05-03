cb925c1961c30fe6df03e9ea374b8d593c559d7e
Dowglas Maia <dowglasmaia@gmail.com>
2019-05-02 21:20:03 -0300

    Criando Ordem Compra Services

---
+++ src/app/services/ordem-compra.service.ts
@@ -0,0 +1,25 @@
+import { Oferta } from '../shared/oferta';
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+
+import { Observable } from 'rxjs';
+import { map, retry } from 'rxjs/operators';
+
+
+/* importação para convertere de Observable para Promisse*/
+
+
+
+@Injectable()
+export class OrdemCompraServices {
+
+  constructor(
+    private http: HttpClient) { }
+
+  public efetivarCompra() {
+    console.log("Comessando a impl Compra") 
+   }
+
+}
